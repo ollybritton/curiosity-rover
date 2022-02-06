165f81317e71e335b6b3455cc4ea5ad3bc7f9e67
Olly Britton <ollybritton@gmail.com>
2022-02-06 19:10:10 +0000

    upload project to github

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,133 @@
 /*jshint esversion: 6 */
+const initial_image = {
+    sol_number: 100,
+    image_number: 0
+};
+
+const image = elem("image");
+const pre_image = elem("pre-image");
+
+const sol_number_display = elem("sol-number-display");
+const small_sol_number = elem("small-sol-number");
+const picture_number = elem("picture-number");
+const nasa_link = elem("nasa-link");
+
+const info_text = elem("text");
+
+let current_image = initial_image;
+let slider_check_amount = 0;
+
+//const slider = elem("slider");
+
+// =============================================
+
+function set_current(load_behind = next_image) {
+    console.log("Setting current");
+    console.log(sol_name(current_image));
+    set_image(image, sol_name(current_image));
+    console.log(sol_name(load_behind(current_image)));
+    set_image(pre_image, sol_name(load_behind(current_image)));
+
+    sol_number_display.text(current_image.sol_number);
+    small_sol_number.text(current_image.sol_number);
+
+    picture_number.text(`Picture ${current_image.image_number}`);
+    prev_info = previous_sol_info(current_image);
+
+    info_text.html(prev_info.text);
+    nasa_link.attr("href", prev_info.url);
+}
+
+function set_next(amount = 1) {
+    current_image = next_image(current_image, amount);
+    set_current(next_image);
+
+    prev_info = previous_sol_info(current_image);
+
+
+    info_text.html(prev_info.text);
+    nasa_link.attr("href", prev_info.url);
+}
+
+function set_previous(amount = 1) {
+    current_image = previous_image(current_image, amount);
+    set_current(previous_image);
+
+    prev_info = previous_sol_info(current_image);
+
+    info_text.html(prev_info.text);
+    nasa_link.attr("href", prev_info.url);
+}
+
+function set_random() {
+    current_image = next_image({
+        sol_number: randomInt(20, 2210),
+        image_number: 0
+    });
+    set_current();
+}
+
+// function set_slider() {
+//     console.log("Setting image based on slider value...", slider.val())
+//     current_image = {
+//         sol_number: slider.val(),
+//         image_number: 0
+//     };
+
+//     console.log("Corresponding image object", current_image)
+
+//     current_image = next_image(current_image)
+
+//     console.log("Next image called to avoid not found errors", current_image);
+//     set_current();
+// }
+
+// =============================================
+
+document.addEventListener("keydown", e => {
+    if (e.key == "ArrowRight") {
+        // Move 1 ahead.
+        set_next();
+    } else if (e.key == "ArrowLeft") {
+        // Move 1 behind.
+        set_previous();
+    }
+
+    if (e.key == "p") {
+        // Move 1 ahead.
+        set_next(1);
+    } else if (e.key == "i") {
+        // Move 1 behind.
+        set_previous(1);
+    }
+
+    if (e.key == "l") {
+        // Move 10 ahead.
+        set_next(10);
+    } else if (e.key == "j") {
+        // Move 10 behind.
+        set_previous(10);
+    }
+
+    if (e.key == "m") {
+        // Move 100 ahead.
+        set_next(100);
+    } else if (e.key == "b") {
+        // Move 100 behind.
+        set_previous(100);
+    }
+
+    if (e.key == "r") {
+        // Jump to a random point.
+        set_random();
+    }
+});
+
+// slider.bind("change", function () {
+//     console.log(slider.val());
+//     set_slider();
+// });
+
+(function () {
+    set_random();
+})();
--- assets/js/misc/tools.js
+++ assets/js/misc/tools.js
@@ -1,10 +1,141 @@
 /*jshint esversion: 6 */
 
-function elem(tag) {
-  return document.getElementsById(tag);
+function elem(target) {
+  return $("[js-target='" + target + "']");
 }
 
-function show(elem, type="flex") {
+function set_image(element, name) {
+  console.log("In set_image function, with values", element, name)
+  name = "url(../../assets/img/" + name + ")";
+  console.log(name);
+  element.css("background-image", name);
+  console.log(element.css("background-image"))
+}
+
+function sol_name(image_data, type = "compressed/front_avoidance") {
+  let sol_number = image_data.sol_number;
+  let image_number = image_data.image_number;
+
+  return type + "/sol_" + sol_number + "_image_" + image_number + ".jpg";
+}
+
+function avaliable_images(sol_number) {
+  let picture_numbers = sol_data[String(sol_number)];
+
+  if (picture_numbers == undefined) {
+    return [];
+  } else {
+    return picture_numbers;
+  }
+}
+
+function next_image(image_data, amount = 1) {
+  for (var i = 0; i < amount; i++) {
+    console.log("In next_image function with parameters:", image_data, amount)
+
+    let sol_number = image_data.sol_number;
+    console.log(sol_number)
+    let image_number = image_data.image_number;
+    console.log(image_number)
+
+    let avaliable = avaliable_images(sol_number);
+    console.log(avaliable)
+
+    if (sol_number > 2210) {
+      return {
+        sol_number: 2210,
+        image_number: 0
+      };
+    }
+
+    if (sol_number < 14) {
+      return {
+        sol_number: 15,
+        image_number: 0
+      };
+    }
+
+    if (avaliable.indexOf(image_number + 1) == -1) {
+      next = 1;
+
+      while (JSON.stringify(avaliable_images(sol_number + next)) == "[]" && sol_number + next < 2210 && sol_number + next > 0) {
+        console.log(next, JSON.stringify(avaliable_images(sol_number + next)));
+        next += 1;
+      }
+
+      image_data = {
+        sol_number: sol_number + next,
+        image_number: 0
+      };
+
+    } else {
+      image_data = {
+        sol_number: sol_number,
+        image_number: image_number + 1
+      };
+    }
+  }
+
+  return image_data;
+}
+
+function previous_sol_info(image_data) {
+  let sol = image_data.sol_number;
+
+  let subtract_amount = 0;
+
+  while (day_info[sol - subtract_amount] == undefined) {
+    subtract_amount += 1;
+  }
+
+  return day_info[sol - subtract_amount]
+}
+
+function previous_image(image_data, amount = 1) {
+  for (var i = 0; i < amount; i++) {
+    let sol_number = image_data.sol_number;
+    let image_number = image_data.image_number;
+
+    let avaliable = avaliable_images(sol_number);
+
+    if (sol_number > 2210) {
+      return {
+        sol_number: 2210,
+        image_number: 0
+      };
+    }
+
+    if (sol_number < 14) {
+      return {
+        sol_number: 15,
+        image_number: 0
+      };
+    }
+
+    if (image_number - 1 < 0) {
+      next = 1;
+
+      while (avaliable_images(sol_number - next)[0] == undefined && sol_number - next < 2210 && sol_number - next > 13) {
+        next += 1;
+      }
+
+      image_data = {
+        sol_number: sol_number - next,
+        image_number: avaliable_images(sol_number - next)[avaliable_images(sol_number - next).length - 1]
+      };
+
+    } else {
+      image_data = {
+        sol_number: sol_number,
+        image_number: image_number - 1
+      };
+    }
+  }
+
+  return image_data;
+}
+
+function show(elem, type = "flex") {
   elem.style.display = type;
 }
 
@@ -25,5 +156,5 @@ function toggleClass(elem, val) {
 }
 
 function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min +1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
